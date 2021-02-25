ce249dfb520f6a40d54cc1141ae09eb6ed202a70
Darren Reid <darren@reidmail.org>
2021-02-25 15:40:05 +1100

    Initial commit

---
+++ SelfHostTemplate.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+using System;
+using ServiceStack;
+using SelfHostTemplate.ServiceModel;
+
+namespace SelfHostTemplate.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
---
+++ SelfHostTemplate/Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Funq;
+using Microsoft.Extensions.Configuration;
+using ServiceStack;
+using SelfHostTemplate.ServiceInterface;
+using SelfHostTemplate.ServiceModel;
+
+namespace SelfHostTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseModularStartup<Startup>()
+                .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000/")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup : ModularStartup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public new void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
+            {
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
+            });
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("SelfHostTemplate", typeof(MyServices).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+}
